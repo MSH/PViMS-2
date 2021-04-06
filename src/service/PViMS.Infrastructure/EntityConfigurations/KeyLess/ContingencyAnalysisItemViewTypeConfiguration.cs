17085112e178535d701a4c3cb2332454891f80dd
Shaun Krog <shaun.krog@outlook.com>
2021-04-06 13:56:33 +0200

    add column type and precision for contingency keyless entity #AB79

--- src/service/PViMS.Infrastructure/EntityConfigurations/KeyLess/ContingencyAnalysisItemViewTypeConfiguration.cs
+++ src/service/PViMS.Infrastructure/EntityConfigurations/KeyLess/ContingencyAnalysisItemViewTypeConfiguration.cs
@@ -11,6 +11,38 @@ namespace PVIMS.Infrastructure.EntityConfigurations.KeyLess
             configuration.ToView("vwContingencyAnalysisItem");
 
             configuration.HasNoKey();
+
+            configuration.Property(e => e.AdjustedRelativeRisk)
+                .HasColumnType("Decimal")
+                .HasPrecision(9, 2);
+
+            configuration.Property(e => e.UnadjustedRelativeRisk)
+                .HasColumnType("Decimal")
+                .HasPrecision(9, 2);
+
+            configuration.Property(e => e.ConfidenceIntervalHigh)
+                .HasColumnType("Decimal")
+                .HasPrecision(9, 2);
+
+            configuration.Property(e => e.ConfidenceIntervalLow)
+                .HasColumnType("Decimal")
+                .HasPrecision(9, 2);
+
+            configuration.Property(e => e.ExposedIncidenceRate)
+                .HasColumnType("Decimal")
+                .HasPrecision(9, 2);
+
+            configuration.Property(e => e.ExposedPopulation)
+                .HasColumnType("Decimal")
+                .HasPrecision(9, 2);
+
+            configuration.Property(e => e.NonExposedIncidenceRate)
+                .HasColumnType("Decimal")
+                .HasPrecision(9, 2);
+
+            configuration.Property(e => e.NonExposedPopulation)
+                .HasColumnType("Decimal")
+                .HasPrecision(9, 2);
         }
     }
 }
