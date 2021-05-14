d3863e19b5309d6c318129f0a8f2fa743ec51646
Shaun Krog <shaun.krog@outlook.com>
2021-05-14 18:12:43 +0200

    cater for patient medication queries

--- src/service/PViMS.API/Controllers/PatientMedicationsController.cs
+++ src/service/PViMS.API/Controllers/PatientMedicationsController.cs
@@ -1,21 +1,15 @@
-﻿using AutoMapper;
-using MediatR;
+﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PVIMS.API.Application.Commands.PatientAggregate;
+using PVIMS.API.Application.Queries.PatientAggregate;
 using PVIMS.API.Infrastructure.Attributes;
 using PVIMS.API.Infrastructure.Auth;
-using PVIMS.API.Infrastructure.Services;
 using PVIMS.API.Models;
-using PVIMS.Core.CustomAttributes;
-using PVIMS.Core.Entities;
-using PVIMS.Core.Repositories;
-using PVIMS.Core.Services;
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace PVIMS.API.Controllers
@@ -26,30 +20,12 @@ namespace PVIMS.API.Controllers
     public class PatientMedicationsController : ControllerBase
     {
         private readonly IMediator _mediator;
-        private readonly ITypeExtensionHandler _modelExtensionBuilder;
-        private readonly IRepositoryInt<PatientMedication> _patientMedicationRepository;
-        private readonly IRepositoryInt<SelectionDataItem> _selectionDataItemRepository;
-        private readonly IMapper _mapper;
-        private readonly ILinkGeneratorService _linkGeneratorService;
-        private readonly ICustomAttributeService _customAttributeService;
         private readonly ILogger<PatientMedicationsController> _logger;
 
         public PatientMedicationsController(IMediator mediator,
-            IMapper mapper,
-            ILinkGeneratorService linkGeneratorService,
-            ITypeExtensionHandler modelExtensionBuilder,
-            IRepositoryInt<PatientMedication> patientMedicationRepository,
-            IRepositoryInt<SelectionDataItem> selectionDataItemRepository,
-            ICustomAttributeService customAttributeService,
             ILogger<PatientMedicationsController> logger)
         {
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-            _linkGeneratorService = linkGeneratorService ?? throw new ArgumentNullException(nameof(linkGeneratorService));
-            _modelExtensionBuilder = modelExtensionBuilder ?? throw new ArgumentNullException(nameof(modelExtensionBuilder));
-            _patientMedicationRepository = patientMedicationRepository ?? throw new ArgumentNullException(nameof(patientMedicationRepository));
-            _selectionDataItemRepository = selectionDataItemRepository ?? throw new ArgumentNullException(nameof(selectionDataItemRepository));
-            _customAttributeService = customAttributeService ?? throw new ArgumentNullException(nameof(customAttributeService));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
@@ -65,15 +41,23 @@ namespace PVIMS.API.Controllers
         [Produces("application/vnd.pvims.identifier.v1+json", "application/vnd.pvims.identifier.v1+xml")]
         [RequestHeaderMatchesMediaType("Accept",
             "application/vnd.pvims.identifier.v1+json", "application/vnd.pvims.identifier.v1+xml")]
-        public async Task<ActionResult<PatientMedicationIdentifierDto>> GetPatientMedicationByIdentifier(long patientId, long id)
+        public async Task<ActionResult<PatientMedicationIdentifierDto>> GetPatientMedicationByIdentifier(int patientId, int id)
         {
-            var mappedPatientMedication = await GetPatientMedicationAsync<PatientMedicationIdentifierDto>(patientId, id);
-            if (mappedPatientMedication == null)
+            var query = new PatientMedicationIdentifierQuery(patientId, id);
+
+            _logger.LogInformation(
+                "----- Sending query: PatientMedicationIdentifierQuery - {patientId} : {id}",
+                patientId,
+                id);
+
+            var queryResult = await _mediator.Send(query);
+
+            if (queryResult == null)
             {
-                return NotFound();
+                return BadRequest("Query not created");
             }
 
-            return Ok(CreateLinksForPatientMedication<PatientMedicationIdentifierDto>(mappedPatientMedication));
+            return Ok(queryResult);
         }
 
         /// <summary>
@@ -89,15 +73,23 @@ namespace PVIMS.API.Controllers
         [RequestHeaderMatchesMediaType("Accept",
             "application/vnd.pvims.detail.v1+json", "application/vnd.pvims.detail.v1+xml")]
         [ApiExplorerSettings(IgnoreApi = true)]
-        public async Task<ActionResult<PatientMedicationDetailDto>> GetPatientMedicationByDetail(long patientId, long id)
+        public async Task<ActionResult<PatientMedicationDetailDto>> GetPatientMedicationByDetail(int patientId, int id)
         {
-            var mappedPatientMedication = await GetPatientMedicationAsync<PatientMedicationDetailDto>(patientId, id);
-            if (mappedPatientMedication == null)
+            var query = new PatientMedicationDetailQuery(patientId, id);
+
+            _logger.LogInformation(
+                "----- Sending query: PatientMedicationDetailQuery - {patientId} : {id}",
+                patientId,
+                id);
+
+            var queryResult = await _mediator.Send(query);
+
+            if (queryResult == null)
             {
-                return NotFound();
+                return BadRequest("Query not created");
             }
 
-            return Ok(CreateLinksForPatientMedication<PatientMedicationDetailDto>(CustomPatientMedicationMap(mappedPatientMedication)));
+            return Ok(queryResult);
         }
 
         /// <summary>
@@ -207,84 +199,6 @@ namespace PVIMS.API.Controllers
             }
 
             return Ok();
-        }
-
-        /// <summary>
-        /// Get single patient medication from repository and auto map to Dto
-        /// </summary>
-        /// <typeparam name="T">Identifier or detail Dto</typeparam>
-        /// <param name="patientId">Parent resource id to search by</param>
-        /// <param name="id">Resource id to search by</param>
-        /// <returns></returns>
-        private async Task<T> GetPatientMedicationAsync<T>(long patientId, long id) where T : class
-        {
-            var patientMedicationFromRepo = await _patientMedicationRepository.GetAsync(pc => pc.Patient.Id == patientId && pc.Id == id);
-
-            if (patientMedicationFromRepo != null)
-            {
-                // Map EF entity to Dto
-                var mappedPatientMedication = _mapper.Map<T>(patientMedicationFromRepo);
-
-                return mappedPatientMedication;
-            }
-
-            return null;
-        }
-
-        /// <summary>
-        ///  Map additional dto detail elements not handled through automapper
-        /// </summary>
-        /// <param name="dto">The dto that the link has been added to</param>
-        /// <returns></returns>
-        private PatientMedicationDetailDto CustomPatientMedicationMap(PatientMedicationDetailDto dto)
-        {
-            var patientMedication = _patientMedicationRepository.Get(p => p.Id == dto.Id);
-            if (patientMedication == null)
-            {
-                return dto;
-            }
-            IExtendable patientMedicationExtended = patientMedication;
-
-            // Map all custom attributes
-            dto.MedicationAttributes = _modelExtensionBuilder.BuildModelExtension(patientMedicationExtended)
-                .Select(h => new AttributeValueDto()
-                {
-                    Key = h.AttributeKey,
-                    Value = h.TransformValueToString(),
-                    Category = h.Category,
-                    SelectionValue = (h.Type == CustomAttributeType.Selection) ? GetSelectionValue(h.AttributeKey, h.Value.ToString()) : string.Empty
-                }).Where(s => (s.Value != "0" && !String.IsNullOrWhiteSpace(s.Value)) || !String.IsNullOrWhiteSpace(s.SelectionValue)).ToList();
-
-            dto.IndicationType = _customAttributeService.GetCustomAttributeValue("PatientMedication", "Type of Indication", patientMedicationExtended);
-
-            return dto;
-        }
-
-        /// <summary>
-        ///  Prepare HATEOAS links for a single resource
-        /// </summary>
-        /// <param name="dto">The dto that the link has been added to</param>
-        /// <returns></returns>
-        private PatientMedicationIdentifierDto CreateLinksForPatientMedication<T>(T dto)
-        {
-            PatientMedicationIdentifierDto identifier = (PatientMedicationIdentifierDto)(object)dto;
-
-            identifier.Links.Add(new LinkDto(_linkGeneratorService.CreateResourceUri("PatientMedication", identifier.Id), "self", "GET"));
-
-            return identifier;
-        }
-
-        /// <summary>
-        /// Get the corresponding selection value
-        /// </summary>
-        /// <param name="attributeKey">The custom attribute key look up value</param>
-        /// <param name="selectionKey">The selection key look up value</param>
-        /// <returns></returns>
-        private string GetSelectionValue(string attributeKey, string selectionKey)
-        {
-            var selectionitem = _selectionDataItemRepository.Get(s => s.AttributeKey == attributeKey && s.SelectionKey == selectionKey);
-
-            return (selectionitem == null) ? string.Empty : selectionitem.Value;
         }
     }
 }
