2c1b1ed09b97cd5cfa43a1932b3adf884bec0145
Shaun Krog <shaun.krog@outlook.com>
2021-05-24 12:27:16 +0200

    cater for patient search by condition term AB#65

---
+++ src/service/PViMS.API/Application/Models/Parameters/PatientByConditionResourceParameters.cs
@@ -0,0 +1,15 @@
+﻿namespace PVIMS.API.Models.Parameters
+{
+    public class PatientByConditionResourceParameters : BaseResourceParameters
+    {
+        /// <summary>
+        /// Filter patients by custom attribute key
+        /// </summary>
+        public string CustomAttributeKey { get; set; } = "";
+
+        /// <summary>
+        /// Filter patients by custom attribute value
+        /// </summary>
+        public string CustomAttributeValue { get; set; } = "";
+    }
+}
---
+++ src/service/PViMS.API/Application/Queries/PatientAggregate/PatientExpandedByConditionTermQuery.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using PVIMS.API.Models;
+using System.Runtime.Serialization;
+
+namespace PVIMS.API.Application.Queries.PatientAggregate
+{
+    [DataContract]
+    public class PatientExpandedByConditionTermQuery
+        : IRequest<PatientExpandedDto>
+    {
+        [DataMember]
+        public string CustomAttributeKey { get; private set; }
+
+        [DataMember]
+        public string CustomAttributeValue { get; private set; }
+
+        public PatientExpandedByConditionTermQuery()
+        {
+        }
+
+        public PatientExpandedByConditionTermQuery(string customAttributeKey, string customAttributeValue) : this()
+        {
+            CustomAttributeKey = customAttributeKey;
+            CustomAttributeValue = customAttributeValue;
+        }
+    }
+}
