e353aa46b79308018b706a1ef2c655ea39fcd567
Shaun Krog <shaun.krog@outlook.com>
2021-07-16 12:28:26 +0200

    fixed eager loading when adding task comment

--- src/service/PViMS.API/Application/Commands/ReportInstanceAggregate/AddCommentToReportInstanceTaskCommandHandler.cs
+++ src/service/PViMS.API/Application/Commands/ReportInstanceAggregate/AddCommentToReportInstanceTaskCommandHandler.cs
@@ -39,7 +39,7 @@ namespace PVIMS.API.Application.Commands.ReportInstanceAggregate
         {
             var reportInstanceFromRepo = await _reportInstanceRepository.GetAsync(ri => ri.WorkFlow.WorkFlowGuid == message.WorkFlowGuid
                     && ri.Id == message.ReportInstanceId,
-                    new string[] { "Tasks.CreatedBy", "Tasks.Comments", "CreatedBy", "WorkFlow" });
+                    new string[] { "Tasks.CreatedBy", "Tasks.Comments.CreatedBy", "CreatedBy", "WorkFlow" });
 
             if(reportInstanceFromRepo == null)
             {
