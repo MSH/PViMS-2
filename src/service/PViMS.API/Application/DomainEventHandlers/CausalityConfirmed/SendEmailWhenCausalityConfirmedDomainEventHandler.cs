711e2efb5a902c3d50ae08b81863de5a93c7405c
Shaun Krog <shaun.krog@outlook.com>
2021-07-16 14:16:32 +0200

    include additional information on causality confirmed email domain handler AB#14

--- src/service/PViMS.API/Application/DomainEventHandlers/CausalityConfirmed/SendEmailWhenCausalityConfirmedDomainEventHandler.cs
+++ src/service/PViMS.API/Application/DomainEventHandlers/CausalityConfirmed/SendEmailWhenCausalityConfirmedDomainEventHandler.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using PViMS.Core.Events;
 using PVIMS.API.Infrastructure.Services;
+using PVIMS.Core.Aggregates.ReportInstanceAggregate;
 using System;
 using System.Text;
 using System.Threading;
@@ -30,6 +31,12 @@ namespace PVIMS.API.Application.DomainEventHandlers.TaskAdded
             sb.Append($"<tr><td style='padding: 10px; border: 1px solid black;'><b>Patient</b></td><td style='padding: 10px; border: 1px solid black;'>{domainEvent.ReportInstance.PatientIdentifier}</td></tr>");
             sb.Append($"<tr><td style='padding: 10px; border: 1px solid black;'><b>Created</b></td><td style='padding: 10px; border: 1px solid black;'>{domainEvent.ReportInstance.Created}</td></tr>");
             sb.Append($"<tr><td style='padding: 10px; border: 1px solid black;'><b>Description</b></td><td style='padding: 10px; border: 1px solid black;'>{domainEvent.ReportInstance.SourceIdentifier}</td></tr>");
+            sb.Append("</table>");
+            sb.Append("<p><b><u>Causality and Terminology Details</u></b></p>");
+            sb.Append("<table>");
+            sb.Append($"<tr><td style='padding: 10px; border: 1px solid black;'><b>Terminology</b></td><td style='padding: 10px; border: 1px solid black;'>{domainEvent.ReportInstance.TerminologyMedDra.DisplayName}</td></tr>");
+            sb.Append($"<tr><td style='padding: 10px; border: 1px solid black;'><b>Classification</b></td><td style='padding: 10px; border: 1px solid black;'>{ReportClassification.From(domainEvent.ReportInstance.ReportClassificationId)}</td></tr>");
+            sb.Append($"<tr><td style='padding: 10px; border: 1px solid black;'><b>Causality</b></td><td style='padding: 10px; border: 1px solid black;'>Please browse PV feedback for more details</td></tr>");
             sb.Append("</table>");
 
             await _smtpMailService.SendEmailAsync(subject, sb.ToString(), domainEvent.ReportInstance.CreatedBy.FullName, domainEvent.ReportInstance.CreatedBy.Email );
