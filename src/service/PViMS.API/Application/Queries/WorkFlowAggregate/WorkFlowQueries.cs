e54aceadfc8351917c5e8a402c78bfe73361e9b7
Shaun Krog <shaun.krog@outlook.com>
2021-08-04 19:23:46 +0200

    update work flow summary query handler to handle summarised classifications AB#82

--- src/service/PViMS.API/Application/Queries/WorkFlowAggregate/WorkFlowQueries.cs
+++ src/service/PViMS.API/Application/Queries/WorkFlowAggregate/WorkFlowQueries.cs
@@ -66,53 +66,57 @@ namespace PVIMS.API.Application.Queries.WorkFlowAggregate
 						WHERE wf.WorkFlowGuid = '{workFlowGuid}'");
 
 				workFlowSummary.Classifications = await connection.QueryAsync<ClassificationSummaryDto>(
-					@$"SELECT  'AESI' AS [Classification],
-								CASE WHEN EXISTS (SELECT rim.Id FROM ReportInstanceMedication rim WHERE rim.ReportInstance_Id = ri.Id AND (rim.[NaranjoCausality] IN ('Definite', 'Probable', 'Possible') OR rim.[WHOCausality] IN ('Certain', 'Probable', 'Possible'))) THEN 1 ELSE 0 END AS HasCausality,
-								CASE WHEN EXISTS (SELECT evt.Id FROM ActivityExecutionStatusEvent evt INNER JOIN ActivityExecutionStatus aes ON evt.ExecutionStatus_Id = aes.Id WHERE evt.ActivityInstance_Id = ai.Id AND aes.[Description] = 'E2BSUBMITTED') THEN 1 ELSE 0 END AS E2BSubmitted
-						FROM ReportInstance ri 
-							INNER JOIN WorkFlow wf ON ri.WorkFlow_Id = wf.Id
-							INNER JOIN ActivityInstance ai ON ai.ReportInstance_Id = ri.Id AND ai.[Current] = 1
-							INNER JOIN ActivityExecutionStatus aes ON ai.CurrentStatus_Id = aes.Id
-						WHERE wf.WorkFlowGuid = '{workFlowGuid}'
-							AND ri.FacilityIdentifier IN ({facilityCodeList})
-							AND ri.ReportClassificationId = 1
-							AND ai.QualifiedName = 'Extract E2B'
-						UNION ALL 
-						SELECT  'SAE' AS [Classification],
-								CASE WHEN EXISTS (SELECT rim.Id FROM ReportInstanceMedication rim WHERE rim.ReportInstance_Id = ri.Id AND (rim.[NaranjoCausality] IN ('Definite', 'Probable', 'Possible') OR rim.[WHOCausality] IN ('Certain', 'Probable', 'Possible'))) THEN 1 ELSE 0 END AS HasCausality,
-								CASE WHEN EXISTS (SELECT evt.Id FROM ActivityExecutionStatusEvent evt INNER JOIN ActivityExecutionStatus aes ON evt.ExecutionStatus_Id = aes.Id WHERE evt.ActivityInstance_Id = ai.Id AND aes.[Description] = 'E2BSUBMITTED') THEN 1 ELSE 0 END AS E2BSubmitted
-						FROM ReportInstance ri 
-							INNER JOIN WorkFlow wf ON ri.WorkFlow_Id = wf.Id
-							INNER JOIN ActivityInstance ai ON ai.ReportInstance_Id = ri.Id AND ai.[Current] = 1
-							INNER JOIN ActivityExecutionStatus aes ON ai.CurrentStatus_Id = aes.Id
-						WHERE wf.WorkFlowGuid = '{workFlowGuid}'
-							AND ri.FacilityIdentifier IN ({facilityCodeList})
-							AND ri.ReportClassificationId = 2
-							AND ai.QualifiedName = 'Extract E2B'
-						UNION ALL 
-						SELECT  'Clinically Significant' AS [Classification],
-								CASE WHEN EXISTS (SELECT rim.Id FROM ReportInstanceMedication rim WHERE rim.ReportInstance_Id = ri.Id AND (rim.[NaranjoCausality] IN ('Definite', 'Probable', 'Possible') OR rim.[WHOCausality] IN ('Certain', 'Probable', 'Possible'))) THEN 1 ELSE 0 END AS HasCausality,
-								CASE WHEN EXISTS (SELECT evt.Id FROM ActivityExecutionStatusEvent evt INNER JOIN ActivityExecutionStatus aes ON evt.ExecutionStatus_Id = aes.Id WHERE evt.ActivityInstance_Id = ai.Id AND aes.[Description] = 'E2BSUBMITTED') THEN 1 ELSE 0 END AS E2BSubmitted
-						FROM ReportInstance ri 
-							INNER JOIN WorkFlow wf ON ri.WorkFlow_Id = wf.Id
-							INNER JOIN ActivityInstance ai ON ai.ReportInstance_Id = ri.Id AND ai.[Current] = 1
-							INNER JOIN ActivityExecutionStatus aes ON ai.CurrentStatus_Id = aes.Id
-						WHERE wf.WorkFlowGuid = '{workFlowGuid}'
-							AND ri.FacilityIdentifier IN ({facilityCodeList})
-							AND ri.ReportClassificationId = 3
-							AND ai.QualifiedName = 'Extract E2B'
-						UNION ALL 
-						SELECT  'Unclassified' AS [Classification],
-								CASE WHEN EXISTS (SELECT rim.Id FROM ReportInstanceMedication rim WHERE rim.ReportInstance_Id = ri.Id AND (rim.[NaranjoCausality] IN ('Definite', 'Probable', 'Possible') OR rim.[WHOCausality] IN ('Certain', 'Probable', 'Possible'))) THEN 1 ELSE 0 END AS HasCausality,
-								CASE WHEN EXISTS (SELECT evt.Id FROM ActivityExecutionStatusEvent evt INNER JOIN ActivityExecutionStatus aes ON evt.ExecutionStatus_Id = aes.Id WHERE evt.ActivityInstance_Id = ai.Id AND aes.[Description] = 'E2BSUBMITTED') THEN 1 ELSE 0 END AS E2BSubmitted
-						FROM ReportInstance ri 
-							INNER JOIN WorkFlow wf ON ri.WorkFlow_Id = wf.Id
-							INNER JOIN ActivityInstance ai ON ai.ReportInstance_Id = ri.Id AND ai.[Current] = 1
-							INNER JOIN ActivityExecutionStatus aes ON ai.CurrentStatus_Id = aes.Id
-						WHERE wf.WorkFlowGuid = '{workFlowGuid}'
-							AND ri.FacilityIdentifier IN ({facilityCodeList})
-							AND ri.ReportClassificationId = 4
-							AND ai.QualifiedName = 'Extract E2B'");
+					@$"SELECT sub.[Classification], COUNT(*) AS ClassificationCount, SUM(sub.HasCausality) AS CausativeCount, SUM(sub.E2BSubmitted) AS E2BCount
+							FROM 
+								(SELECT  'AESI' AS [Classification],
+										CASE WHEN EXISTS (SELECT rim.Id FROM ReportInstanceMedication rim WHERE rim.ReportInstance_Id = ri.Id AND (rim.[NaranjoCausality] IN ('Definite', 'Probable', 'Possible') OR rim.[WHOCausality] IN ('Certain', 'Probable', 'Possible'))) THEN 1 ELSE 0 END AS HasCausality,
+										CASE WHEN EXISTS (SELECT evt.Id FROM ActivityExecutionStatusEvent evt INNER JOIN ActivityExecutionStatus aes ON evt.ExecutionStatus_Id = aes.Id WHERE evt.ActivityInstance_Id = ai.Id AND aes.[Description] = 'E2BSUBMITTED') THEN 1 ELSE 0 END AS E2BSubmitted
+								FROM ReportInstance ri 
+									INNER JOIN WorkFlow wf ON ri.WorkFlow_Id = wf.Id
+									INNER JOIN ActivityInstance ai ON ai.ReportInstance_Id = ri.Id AND ai.[Current] = 1
+									INNER JOIN ActivityExecutionStatus aes ON ai.CurrentStatus_Id = aes.Id
+								WHERE wf.WorkFlowGuid = '{workFlowGuid}'
+									AND ri.FacilityIdentifier IN ({facilityCodeList})
+									AND ri.ReportClassificationId = 1
+									AND ai.QualifiedName = 'Extract E2B'
+								UNION ALL 
+								SELECT  'SAE' AS [Classification],
+										CASE WHEN EXISTS (SELECT rim.Id FROM ReportInstanceMedication rim WHERE rim.ReportInstance_Id = ri.Id AND (rim.[NaranjoCausality] IN ('Definite', 'Probable', 'Possible') OR rim.[WHOCausality] IN ('Certain', 'Probable', 'Possible'))) THEN 1 ELSE 0 END AS HasCausality,
+										CASE WHEN EXISTS (SELECT evt.Id FROM ActivityExecutionStatusEvent evt INNER JOIN ActivityExecutionStatus aes ON evt.ExecutionStatus_Id = aes.Id WHERE evt.ActivityInstance_Id = ai.Id AND aes.[Description] = 'E2BSUBMITTED') THEN 1 ELSE 0 END AS E2BSubmitted
+								FROM ReportInstance ri 
+									INNER JOIN WorkFlow wf ON ri.WorkFlow_Id = wf.Id
+									INNER JOIN ActivityInstance ai ON ai.ReportInstance_Id = ri.Id AND ai.[Current] = 1
+									INNER JOIN ActivityExecutionStatus aes ON ai.CurrentStatus_Id = aes.Id
+								WHERE wf.WorkFlowGuid = '{workFlowGuid}'
+									AND ri.FacilityIdentifier IN ({facilityCodeList})
+									AND ri.ReportClassificationId = 2
+									AND ai.QualifiedName = 'Extract E2B'
+								UNION ALL 
+								SELECT  'Clinically Significant' AS [Classification],
+										CASE WHEN EXISTS (SELECT rim.Id FROM ReportInstanceMedication rim WHERE rim.ReportInstance_Id = ri.Id AND (rim.[NaranjoCausality] IN ('Definite', 'Probable', 'Possible') OR rim.[WHOCausality] IN ('Certain', 'Probable', 'Possible'))) THEN 1 ELSE 0 END AS HasCausality,
+										CASE WHEN EXISTS (SELECT evt.Id FROM ActivityExecutionStatusEvent evt INNER JOIN ActivityExecutionStatus aes ON evt.ExecutionStatus_Id = aes.Id WHERE evt.ActivityInstance_Id = ai.Id AND aes.[Description] = 'E2BSUBMITTED') THEN 1 ELSE 0 END AS E2BSubmitted
+								FROM ReportInstance ri 
+									INNER JOIN WorkFlow wf ON ri.WorkFlow_Id = wf.Id
+									INNER JOIN ActivityInstance ai ON ai.ReportInstance_Id = ri.Id AND ai.[Current] = 1
+									INNER JOIN ActivityExecutionStatus aes ON ai.CurrentStatus_Id = aes.Id
+								WHERE wf.WorkFlowGuid = '{workFlowGuid}'
+									AND ri.FacilityIdentifier IN ({facilityCodeList})
+									AND ri.ReportClassificationId = 3
+									AND ai.QualifiedName = 'Extract E2B'
+								UNION ALL 
+								SELECT  'Unclassified' AS [Classification],
+										CASE WHEN EXISTS (SELECT rim.Id FROM ReportInstanceMedication rim WHERE rim.ReportInstance_Id = ri.Id AND (rim.[NaranjoCausality] IN ('Definite', 'Probable', 'Possible') OR rim.[WHOCausality] IN ('Certain', 'Probable', 'Possible'))) THEN 1 ELSE 0 END AS HasCausality,
+										CASE WHEN EXISTS (SELECT evt.Id FROM ActivityExecutionStatusEvent evt INNER JOIN ActivityExecutionStatus aes ON evt.ExecutionStatus_Id = aes.Id WHERE evt.ActivityInstance_Id = ai.Id AND aes.[Description] = 'E2BSUBMITTED') THEN 1 ELSE 0 END AS E2BSubmitted
+								FROM ReportInstance ri 
+									INNER JOIN WorkFlow wf ON ri.WorkFlow_Id = wf.Id
+									INNER JOIN ActivityInstance ai ON ai.ReportInstance_Id = ri.Id AND ai.[Current] = 1
+									INNER JOIN ActivityExecutionStatus aes ON ai.CurrentStatus_Id = aes.Id
+								WHERE wf.WorkFlowGuid = '{workFlowGuid}'
+									AND ri.FacilityIdentifier IN ({facilityCodeList})
+									AND ri.ReportClassificationId = 4
+									AND ai.QualifiedName = 'Extract E2B') AS sub
+						GROUP BY sub.[Classification]
+						ORDER BY sub.[Classification]");
 
 				return workFlowSummary;
 			}
