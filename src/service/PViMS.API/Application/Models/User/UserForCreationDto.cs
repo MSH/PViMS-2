b8782cc01e14aeaf55245124a5e4e052f7590cf6
Shaun Krog <shaun.krog@outlook.com>
2021-08-16 13:04:16 +0200

    cater for add user command and handler

--- src/service/PViMS.API/Application/Models/User/UserForCreationDto.cs
+++ src/service/PViMS.API/Application/Models/User/UserForCreationDto.cs
@@ -9,11 +9,8 @@ namespace PVIMS.API.Models
         /// The email address of the user
         /// </summary>
         [EmailAddress]
-        [StringLength(150)]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
@@ -24,22 +21,16 @@ namespace PVIMS.API.Models
         /// <summary>
         /// The username of the user
         /// </summary>
-        [Required]
-        [StringLength(30)]
         public string UserName { get; set; }
 
         /// <summary>
         /// The first name of the user
         /// </summary>
-        [Required]
-        [StringLength(30)]
         public string FirstName { get; set; }
 
         /// <summary>
         /// The last name of the user
         /// </summary>
-        [Required]
-        [StringLength(30)]
         public string LastName { get; set; }
 
         /// <summary>
--- src/service/PViMS.API/Controllers/UsersController.cs
+++ src/service/PViMS.API/Controllers/UsersController.cs
@@ -13,7 +13,6 @@ using PVIMS.API.Helpers;
 using PVIMS.API.Models;
 using PVIMS.API.Models.Parameters;
 using PVIMS.Core.Entities;
-using PVIMS.Core.Entities.Accounts;
 using Extensions = PVIMS.Core.Utilities.Extensions;
 using PVIMS.Core.Repositories;
 using PVIMS.Core.Paging;
@@ -23,6 +22,11 @@ using System.Collections;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using PVIMS.API.Application.Commands.UserAggregate;
+using MediatR;
+using PVIMS.API.Application.Queries.UserAggregate;
+using PVIMS.Core.Aggregates.UserAggregate;
 
 namespace PVIMS.API.Controllers
 {
@@ -31,6 +35,7 @@ namespace PVIMS.API.Controllers
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme + "," + ApiKeyAuthenticationOptions.DefaultScheme)]
     public class UsersController : ControllerBase
     {
+        private readonly IMediator _mediator;
         private readonly ITypeHelperService _typeHelperService;
         private readonly IRepositoryInt<User> _userRepository;
         private readonly IRepositoryInt<UserFacility> _userFacilityRepository;
@@ -39,16 +44,20 @@ namespace PVIMS.API.Controllers
         private readonly IMapper _mapper;
         private readonly ILinkGeneratorService _linkGeneratorService;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<UsersController> _logger;
 
-        public UsersController(ITypeHelperService typeHelperService,
+        public UsersController(IMediator mediator,
+            ITypeHelperService typeHelperService,
             IMapper mapper,
             ILinkGeneratorService linkGeneratorService,
             IRepositoryInt<User> userRepository,
             IRepositoryInt<UserFacility> userFacilityRepository,
             IRepositoryInt<Facility> facilityRepository,
             IUnitOfWorkInt unitOfWork,
-            UserManager<ApplicationUser> userManager)
+            UserManager<ApplicationUser> userManager,
+            ILogger<UsersController> logger)
         {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
             _typeHelperService = typeHelperService ?? throw new ArgumentNullException(nameof(typeHelperService));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
             _linkGeneratorService = linkGeneratorService ?? throw new ArgumentNullException(nameof(linkGeneratorService));
@@ -57,6 +66,7 @@ namespace PVIMS.API.Controllers
             _facilityRepository = facilityRepository ?? throw new ArgumentNullException(nameof(facilityRepository));
             _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
             _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         /// <summary>
@@ -68,7 +78,7 @@ namespace PVIMS.API.Controllers
         [Produces("application/vnd.pvims.identifier.v1+json", "application/vnd.pvims.identifier.v1+xml")]
         [RequestHeaderMatchesMediaType("Accept",
             "application/vnd.pvims.identifier.v1+json", "application/vnd.pvims.identifier.v1+xml")]
-        public ActionResult<LinkedCollectionResourceWrapperDto<UserIdentifierDto>> GetUsersByIdentifier(
+        public async Task<ActionResult<LinkedCollectionResourceWrapperDto<UserIdentifierDto>>> GetUsersByIdentifier(
             [FromQuery] UserResourceParameters userResourceParameters)
         {
             if (!_typeHelperService.TypeHasProperties<UserIdentifierDto>
@@ -77,35 +87,35 @@ namespace PVIMS.API.Controllers
                 return BadRequest();
             }
 
-            var mappedUsersWithLinks = GetUsers<UserIdentifierDto>(userResourceParameters);
+            var query = new UsersIdentifierQuery(
+                userResourceParameters.OrderBy,
+                userResourceParameters.PageNumber,
+                userResourceParameters.PageSize,
+                userResourceParameters.SearchTerm);
 
-            var wrapper = new LinkedCollectionResourceWrapperDto<UserIdentifierDto>(mappedUsersWithLinks.TotalCount, mappedUsersWithLinks);
-            var wrapperWithLinks = CreateLinksForUsers(wrapper, userResourceParameters,
-                mappedUsersWithLinks.HasNext, mappedUsersWithLinks.HasPrevious);
+            _logger.LogInformation(
+                "----- Sending query: UsersIdentifierQuery");
 
-            return Ok(wrapper);
-        }
+            var queryResult = await _mediator.Send(query);
 
-        /// <summary>
-        /// Get a single user using it's unique id and valid media type 
-        /// </summary>
-        /// <param name="id">The unique identifier for the user</param>
-        /// <returns>An ActionResult of type UserIdentifierDto</returns>
-        [HttpGet("{id}", Name = "GetUserByIdentifier")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [Produces("application/vnd.pvims.identifier.v1+json", "application/vnd.pvims.identifier.v1+xml")]
-        [RequestHeaderMatchesMediaType("Accept",
-            "application/vnd.pvims.identifier.v1+json", "application/vnd.pvims.identifier.v1+xml")]
-        public async Task<ActionResult<UserIdentifierDto>> GetUserByIdentifier(long id)
-        {
-            var mappedUser = await GetUserAsync<UserIdentifierDto>(id);
-            if (mappedUser == null)
+            if (queryResult == null)
             {
-                return NotFound();
+                return BadRequest("Query not created");
             }
 
-            return Ok(CreateLinksForUser<UserIdentifierDto>(mappedUser));
+            // Prepare pagination data for response
+            var paginationMetadata = new
+            {
+                totalCount = queryResult.RecordCount,
+                pageSize = userResourceParameters.PageSize,
+                currentPage = userResourceParameters.PageNumber,
+                totalPages = queryResult.PageCount
+            };
+
+            Response.Headers.Add("X-Pagination",
+                JsonConvert.SerializeObject(paginationMetadata));
+
+            return Ok(queryResult);
         }
 
         /// <summary>
@@ -143,6 +153,35 @@ namespace PVIMS.API.Controllers
         /// Get a single user using it's unique id and valid media type 
         /// </summary>
         /// <param name="id">The unique identifier for the user</param>
+        /// <returns>An ActionResult of type UserIdentifierDto</returns>
+        [HttpGet("{id}", Name = "GetUserByIdentifier")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Produces("application/vnd.pvims.identifier.v1+json", "application/vnd.pvims.identifier.v1+xml")]
+        [RequestHeaderMatchesMediaType("Accept",
+            "application/vnd.pvims.identifier.v1+json", "application/vnd.pvims.identifier.v1+xml")]
+        public async Task<ActionResult<UserIdentifierDto>> GetUserByIdentifier(int id)
+        {
+            var query = new UserIdentifierQuery(id);
+
+            _logger.LogInformation(
+                "----- Sending query: UserIdentifierQuery - {id}",
+                id);
+
+            var queryResult = await _mediator.Send(query);
+
+            if (queryResult == null)
+            {
+                return BadRequest("Query not created");
+            }
+
+            return Ok(queryResult);
+        }
+
+        /// <summary>
+        /// Get a single user using it's unique id and valid media type 
+        /// </summary>
+        /// <param name="id">The unique identifier for the user</param>
         /// <returns>An ActionResult of type UserDetailDto</returns>
         [HttpGet("{id}", Name = "GetUserByDetail")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -151,15 +190,22 @@ namespace PVIMS.API.Controllers
         [RequestHeaderMatchesMediaType("Accept",
             "application/vnd.pvims.detail.v1+json", "application/vnd.pvims.detail.v1+xml")]
         [ApiExplorerSettings(IgnoreApi = true)]
-        public async Task<ActionResult<UserDetailDto>> GetUserByDetail(long id)
+        public async Task<ActionResult<UserDetailDto>> GetUserByDetail(int id)
         {
-            var mappedUser = await GetUserAsync<UserDetailDto>(id);
-            if (mappedUser == null)
+            var query = new UserDetailQuery(id);
+
+            _logger.LogInformation(
+                "----- Sending query: UserDetailQuery - {id}",
+                id);
+
+            var queryResult = await _mediator.Send(query);
+
+            if (queryResult == null)
             {
-                return NotFound();
+                return BadRequest("Query not created");
             }
 
-            return Ok(CreateLinksForUser<UserDetailDto>(CustomUserMap(mappedUser)));
+            return Ok(queryResult);
         }
 
         /// <summary>
@@ -179,99 +225,24 @@ namespace PVIMS.API.Controllers
                 return BadRequest(ModelState);
             }
 
-            if (Regex.Matches(userForCreation.UserName, @"[a-zA-Z0-9 ]").Count < userForCreation.UserName.Length)
+            var command = new AddUserCommand(userForCreation.FirstName, userForCreation.LastName, userForCreation.Email, userForCreation.UserName, userForCreation.Password, userForCreation.ConfirmPassword, userForCreation.Roles, userForCreation.Facilities);
+
+            _logger.LogInformation(
+                "----- Sending command: AddUserCommand - {userName}",
+                command.UserName);
+
+            var commandResult = await _mediator.Send(command);
+
+            if (commandResult == null)
             {
-                ModelState.AddModelError("Message", "Value contains invalid characters (Enter A-Z, a-z, 0-9, space)");
+                return BadRequest("Command not created");
             }
 
-            if (Regex.Matches(userForCreation.FirstName, @"[a-zA-Z ]").Count < userForCreation.FirstName.Length)
-            {
-                ModelState.AddModelError("Message", "Value contains invalid characters (Enter A-Z, a-z, space)");
-            }
-
-            if (Regex.Matches(userForCreation.LastName, @"[a-zA-Z ]").Count < userForCreation.LastName.Length)
-            {
-                ModelState.AddModelError("Message", "Value contains invalid characters (Enter A-Z, a-z, space)");
-            }
-
-            if (userForCreation.Facilities == null)
-            {
-                ModelState.AddModelError("Message", "At least one facility must be selected");
-            }
-
-            if (userForCreation.Roles == null)
-            {
-                ModelState.AddModelError("Message", "At least one role must be selected");
-            }
-
-            if (_unitOfWork.Repository<User>().Queryable().
-                Where(l => l.UserName == userForCreation.UserName)
-                .Count() > 0)
-            {
-                ModelState.AddModelError("Message", "Item with same name already exists");
-            }
-
-            if (ModelState.IsValid)
-            {
-                var newApplicationUser = new ApplicationUser() { 
-                    FirstName = userForCreation.FirstName, 
-                    LastName = userForCreation.LastName, 
-                    UserName = userForCreation.UserName, 
-                    Email = userForCreation.Email, 
-                };
-                IdentityResult result = await _userManager.CreateAsync(newApplicationUser, userForCreation.Password);
-                if (result.Succeeded)
+            return CreatedAtAction("GetUserByIdentifier",
+                new
                 {
-                    IdentityResult roleResult = await _userManager.AddToRolesAsync(newApplicationUser, userForCreation.Roles);
-
-                    if (roleResult.Succeeded)
-                    {
-                        var userFromRepo = await _userRepository.GetAsync(f => f.Id == 1);
-                        if (userFromRepo == null)
-                        {
-                            return StatusCode(500, "Unable to locate newly added item");
-                        }
-
-                        // ensure facilities are linked to user
-                        var facilityNames = userForCreation.Facilities.ToArray();
-
-                        foreach (string facilityName in facilityNames)
-                        {
-                            var uf = new UserFacility() { 
-                                Facility = _facilityRepository.Get(f => f.FacilityName == facilityName), 
-                                User = userFromRepo
-                            };
-                            userFromRepo.Facilities.Add(uf);
-                        }
-                        _userRepository.Update(userFromRepo);
-                        await _unitOfWork.CompleteAsync();
-
-                        var mappedUser = _mapper.Map<UserIdentifierDto>(userFromRepo);
-
-                        return CreatedAtAction("GetUserByIdentifier",
-                            new
-                            {
-                                id = mappedUser.Id
-                            }, CreateLinksForUser<UserIdentifierDto>(mappedUser));
-                    }
-                    else
-                    {
-                        foreach (var error in result.Errors)
-                        {
-                            ModelState.AddModelError("Message", $"{error.Description} ({error.Code})");
-                        }
-                    }
-                }
-                else
-                {
-                    foreach (var error in result.Errors)
-                    {
-                        ModelState.AddModelError("Message", $"{error.Description} ({error.Code})");
-                    }
-                }
-            }
-
-            return BadRequest(ModelState);
+                    id = commandResult.Id
+                }, commandResult);
         }
 
         /// <summary>
@@ -450,129 +421,6 @@ namespace PVIMS.API.Controllers
             return NoContent();
         }
 
-        private PagedCollection<T> GetUsers<T>(UserResourceParameters userResourceParameters) where T : class
-        {
-            var pagingInfo = new PagingInfo()
-            {
-                PageNumber = userResourceParameters.PageNumber,
-                PageSize = userResourceParameters.PageSize
-            };
-
-            var orderby = Extensions.GetOrderBy<User>(userResourceParameters.OrderBy, "asc");
-
-            var predicate = PredicateBuilder.New<User>(true);
-
-            if (!String.IsNullOrWhiteSpace(userResourceParameters.SearchTerm))
-            {
-                predicate = predicate.And(f => f.UserName.Contains(userResourceParameters.SearchTerm.Trim()) ||
-                            f.FirstName.Contains(userResourceParameters.SearchTerm.Trim()) ||
-                            f.LastName.Contains(userResourceParameters.SearchTerm.Trim()));
-            }
-
-            var pagedUsersFromRepo = _userRepository.List(pagingInfo, predicate, orderby, "");
-            if (pagedUsersFromRepo != null)
-            {
-                // Map EF entity to Dto
-                var mappedUsers = PagedCollection<T>.Create(_mapper.Map<PagedCollection<T>>(pagedUsersFromRepo),
-                    pagingInfo.PageNumber,
-                    pagingInfo.PageSize,
-                    pagedUsersFromRepo.TotalCount);
-
-                // Prepare pagination data for response
-                var paginationMetadata = new
-                {
-                    totalCount = mappedUsers.TotalCount,
-                    pageSize = mappedUsers.PageSize,
-                    currentPage = mappedUsers.CurrentPage,
-                    totalPages = mappedUsers.TotalPages,
-                };
-
-                Response.Headers.Add("X-Pagination",
-                    JsonConvert.SerializeObject(paginationMetadata));
-
-                // Add HATEOAS links to each individual resource
-                //mappedUsers.ForEach(dto => CreateLinksForFacility(dto));
-
-                return mappedUsers;
-            }
-
-            return null;
-        }
-
-        private async Task<T> GetUserAsync<T>(long id) where T : class
-        {
-            var userFromRepo = await _userRepository.GetAsync(f => f.Id == id);
-
-            if (userFromRepo != null)
-            {
-                // Map EF entity to Dto
-                var mappedUser = _mapper.Map<T>(userFromRepo);
-
-                return mappedUser;
-            }
-
-            return null;
-        }
-
-        private LinkedResourceBaseDto CreateLinksForUsers(
-            LinkedResourceBaseDto wrapper,
-            UserResourceParameters userResourceParameters,
-            bool hasNext, bool hasPrevious)
-        {
-            wrapper.Links.Add(
-               new LinkDto(
-                   _linkGeneratorService.CreateUsersResourceUri(ResourceUriType.Current, userResourceParameters),
-                   "self", "GET"));
-
-            if (hasNext)
-            {
-                wrapper.Links.Add(
-                   new LinkDto(
-                       _linkGeneratorService.CreateUsersResourceUri(ResourceUriType.NextPage, userResourceParameters),
-                       "nextPage", "GET"));
-            }
-
-            if (hasPrevious)
-            {
-                wrapper.Links.Add(
-                   new LinkDto(
-                       _linkGeneratorService.CreateUsersResourceUri(ResourceUriType.PreviousPage, userResourceParameters),
-                       "previousPage", "GET"));
-            }
-
-            return wrapper;
-        }
-
-        /// <summary>
-        ///  Prepare HATEOAS links for a single resource
-        /// </summary>
-        /// <param name="dto">The dto that the link has been added to</param>
-        /// <returns></returns>
-        private UserIdentifierDto CreateLinksForUser<T>(T dto)
-        {
-            UserIdentifierDto identifier = (UserIdentifierDto)(object)dto;
-
-            identifier.Links.Add(new LinkDto(_linkGeneratorService.CreateResourceUri("User", identifier.Id), "self", "GET"));
-
-            return identifier;
-        }
-
-        /// <summary>
-        ///  Map additional dto detail elements not handled through automapper
-        /// </summary>
-        /// <param name="dto">The dto that the link has been added to</param>
-        /// <returns></returns>
-        private UserDetailDto CustomUserMap(UserDetailDto dto)
-        {
-            //dto.Roles = _unitOfWork.Repository<UserRole>().Queryable()
-            //    .Where(ur => ur.User.Id == dto.Id).Select(s => s.Role.Name).ToArray();
-
-            dto.Facilities = _unitOfWork.Repository<UserFacility>().Queryable()
-                .Where(uf => uf.User.Id == dto.Id).Select(s => s.Facility.FacilityName).ToArray();
-
-            return dto;
-        }
-
         /// <summary>
         ///  Handle the updating of roles for an existing usser
         /// </summary>
@@ -664,11 +512,7 @@ namespace PVIMS.API.Controllers
                 if (userFacility == null)
                 {
                     var newFacility = _facilityRepository.Get(r => r.FacilityName == facility);
-                    userFacility = new UserFacility()
-                    {
-                        Facility = newFacility,
-                        User = userFromRepo
-                    };
+                    userFacility = new UserFacility(newFacility, userFromRepo);
                     addCollection.Add(userFacility);
                 }
             }
