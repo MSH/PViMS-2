9e430a4931124e3c975e9fbd7cd80ba6bf0203cd
Shaun Krog <shaun.krog@outlook.com>
2021-09-15 13:05:59 +0200

    environment version number

--- src/web/PViMS.Spa/src/environments/environment.mshtest.ts
+++ src/web/PViMS.Spa/src/environments/environment.mshtest.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiURL: 'https://pvimstest-api.msh.org/api',
-  appVersion: '2.0.0',
+  appVersion: '2.1.0',
   appName: 'PViMS',
   installationDate: '2021-03-31'
 };
