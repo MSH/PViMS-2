e637ba314b0bc24f72867ae1611161f955eaeae5
Shaun Krog <shaun.krog@outlook.com>
2021-09-29 19:11:29 +0200

    update nepal environment file

--- src/web/PViMS.Spa/src/environments/environment.nepal.ts
+++ src/web/PViMS.Spa/src/environments/environment.nepal.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiURL: 'http://103.198.9.145:8095/api',
-  apiURLBase: 'http://103.198.9.145:8095',
+  apiURL: 'http://103.198.9.145:8090/api',
+  apiURLBase: 'http://103.198.9.145:8090',
   appVersion: '2.1.0',
   appName: 'PViMS',
   installationDate: '2021-09-29',
