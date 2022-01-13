c6bb5dcd76758c64e0afb5614b17715b1b44ed98
Shaun Krog <shaun.krog@outlook.com>
2022-01-13 07:13:17 +0200

    cater for causality confirmed integration event

---
+++ src/service/PViMS.API/Application/DomainEventHandlers/CausalityConfirmed/SendIntegrationEventWhenCausalityConfirmedDomainEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using PViMS.Core.Events;
+using PVIMS.API.Application.IntegrationEvents;
+using PVIMS.API.Application.IntegrationEvents.Events;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PVIMS.API.Application.DomainEventHandlers.CausalityConfirmed
+{
+    public class SendIntegrationEventWhenCausalityConfirmedDomainEventHandler
+                            : INotificationHandler<CausalityConfirmedDomainEvent>
+    {
+        private readonly IIntegrationEventService _integrationEventService;
+        private readonly ILogger<SendIntegrationEventWhenCausalityConfirmedDomainEventHandler> _logger;
+
+        public SendIntegrationEventWhenCausalityConfirmedDomainEventHandler(IIntegrationEventService integrationEventService,
+            ILogger<SendIntegrationEventWhenCausalityConfirmedDomainEventHandler> logger)
+        {
+            _integrationEventService = integrationEventService ?? throw new ArgumentNullException(nameof(integrationEventService));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Handle(CausalityConfirmedDomainEvent domainEvent, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Causality confirmed for report instance: {domainEvent.ReportInstance.Id}. Send integration event.");
+
+            var causalityConfirmedIntegrationEvent = new CausalityConfirmedIntegrationEvent(domainEvent.ReportInstance.Id, domainEvent.ReportInstance.Identifier);
+            await _integrationEventService.AddAndSaveEventAsync(causalityConfirmedIntegrationEvent);
+        }
+    }
+}
---
+++ src/service/PViMS.API/Application/IntegrationEvents/Events/CausalityConfirmedIntegrationEvent.cs
@@ -0,0 +1,16 @@
+﻿using PViMS.BuildingBlocks.EventBus.Events;
+
+namespace PVIMS.API.Application.IntegrationEvents.Events
+{
+    public record CausalityConfirmedIntegrationEvent : IntegrationEvent
+    {
+        public int ReportInstanceId { get; }
+        public string Identifier { get; }
+
+        public CausalityConfirmedIntegrationEvent(int reportInstanceId, string identifier)
+        {
+            ReportInstanceId = reportInstanceId;
+            Identifier = identifier;
+        }
+    }
+}
