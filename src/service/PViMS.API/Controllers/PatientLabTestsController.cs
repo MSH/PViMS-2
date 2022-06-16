36df90ac0db07d55dc53278a7557d1f13a9405a1
Shaun Krog <shaun.krog@outlook.com>
2022-06-16 16:03:56 +0200

    cater for patient lab test queries

--- src/service/PViMS.API/Controllers/PatientLabTestsController.cs
+++ src/service/PViMS.API/Controllers/PatientLabTestsController.cs
@@ -10,13 +10,13 @@ using PVIMS.API.Infrastructure.Attributes;
 using PVIMS.API.Infrastructure.Auth;
 using PVIMS.API.Infrastructure.Services;
 using PVIMS.API.Models;
-using PVIMS.Core.CustomAttributes;
 using PVIMS.Core.Entities;
 using PVIMS.Core.Repositories;
 using PVIMS.Core.Services;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using PVIMS.API.Application.Queries.PatientAggregate;
 
 namespace PVIMS.API.Controllers
 {
@@ -25,30 +25,16 @@ namespace PVIMS.API.Controllers
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme + "," + ApiKeyAuthenticationOptions.DefaultScheme)]
     public class PatientLabTestsController : ControllerBase
     {
-        private readonly ITypeExtensionHandler _modelExtensionBuilder;
-        private readonly IRepositoryInt<PatientLabTest> _patientLabTestRepository;
-        private readonly IRepositoryInt<SelectionDataItem> _selectionDataItemRepository;
-        private readonly IMapper _mapper;
         private readonly IMediator _mediator;
         private readonly ILogger<PatientLabTestsController> _logger;
-        private readonly ILinkGeneratorService _linkGeneratorService;
 
         public PatientLabTestsController(
-            IMapper mapper,
             IMediator mediator,
-            ILogger<PatientLabTestsController> logger,
-            ILinkGeneratorService linkGeneratorService,
-            ITypeExtensionHandler modelExtensionBuilder,
-            IRepositoryInt<PatientLabTest> patientLabTestRepository,
-            IRepositoryInt<SelectionDataItem> selectionDataItemRepository)
+            ILogger<PatientLabTestsController> logger
+        )
         {
-            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            _linkGeneratorService = linkGeneratorService ?? throw new ArgumentNullException(nameof(linkGeneratorService));
-            _modelExtensionBuilder = modelExtensionBuilder ?? throw new ArgumentNullException(nameof(modelExtensionBuilder));
-            _patientLabTestRepository = patientLabTestRepository ?? throw new ArgumentNullException(nameof(patientLabTestRepository));
-            _selectionDataItemRepository = selectionDataItemRepository ?? throw new ArgumentNullException(nameof(selectionDataItemRepository));
         }
 
         /// <summary>
@@ -63,15 +49,23 @@ namespace PVIMS.API.Controllers
         [Produces("application/vnd.pvims.identifier.v1+json", "application/vnd.pvims.identifier.v1+xml")]
         [RequestHeaderMatchesMediaType("Accept",
             "application/vnd.pvims.identifier.v1+json", "application/vnd.pvims.identifier.v1+xml")]
-        public async Task<ActionResult<PatientLabTestIdentifierDto>> GetPatientLabTestByIdentifier(long patientId, long id)
+        public async Task<ActionResult<PatientLabTestIdentifierDto>> GetPatientLabTestByIdentifier(int patientId, int id)
         {
-            var mappedPatientLabTest = await GetPatientLabTestAsync<PatientLabTestIdentifierDto>(patientId, id);
-            if (mappedPatientLabTest == null)
+            var query = new PatientLabTestIdentifierQuery(patientId, id);
+
+            _logger.LogInformation(
+                "----- Sending query: PatientLabTestIdentifierQuery - {patientId} : {id}",
+                patientId,
+                id);
+
+            var queryResult = await _mediator.Send(query);
+
+            if (queryResult == null)
             {
-                return NotFound();
+                return BadRequest("Query not created");
             }
 
-            return Ok(CreateLinksForPatientLabTest<PatientLabTestIdentifierDto>(mappedPatientLabTest));
+            return Ok(queryResult);
         }
 
         /// <summary>
@@ -87,15 +81,23 @@ namespace PVIMS.API.Controllers
         [RequestHeaderMatchesMediaType("Accept",
             "application/vnd.pvims.detail.v1+json", "application/vnd.pvims.detail.v1+xml")]
         [ApiExplorerSettings(IgnoreApi = true)]
-        public async Task<ActionResult<PatientLabTestDetailDto>> GetPatientLabTestByDetail(long patientId, long id)
+        public async Task<ActionResult<PatientLabTestDetailDto>> GetPatientLabTestByDetail(int patientId, int id)
         {
-            var mappedPatientLabTest = await GetPatientLabTestAsync<PatientLabTestDetailDto>(patientId, id);
-            if (mappedPatientLabTest == null)
+            var query = new PatientLabTestDetailQuery(patientId, id);
+
+            _logger.LogInformation(
+                "----- Sending query: PatientLabTestDetailQuery - {patientId} : {id}",
+                patientId,
+                id);
+
+            var queryResult = await _mediator.Send(query);
+
+            if (queryResult == null)
             {
-                return NotFound();
+                return BadRequest("Query not created");
             }
 
-            return Ok(CreateLinksForPatientLabTest<PatientLabTestDetailDto>(CustomPatientLabTestMap(mappedPatientLabTest)));
+            return Ok(queryResult);
         }
 
         /// <summary>
@@ -107,7 +109,7 @@ namespace PVIMS.API.Controllers
         [HttpPost("{patientId}/labtests", Name = "CreatePatientLabTest")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [Consumes("application/json")]
-        public async Task<IActionResult> CreatePatientLabTest(int patientId, 
+        public async Task<IActionResult> CreatePatientLabTest(int patientId,
             [FromBody] PatientLabTestForUpdateDto labTestForUpdate)
         {
             if (labTestForUpdate == null)
@@ -163,7 +165,7 @@ namespace PVIMS.API.Controllers
                 return BadRequest(ModelState);
             }
 
-            var command = new ChangeLabTestDetailsCommand(patientId, 
+            var command = new ChangeLabTestDetailsCommand(patientId,
                 id,
                 labTestForUpdate.LabTest,
                 labTestForUpdate.TestDate,
@@ -223,82 +225,6 @@ namespace PVIMS.API.Controllers
             }
 
             return Ok();
-        }
-
-        /// <summary>
-        /// Get single patient lab test from repository and auto map to Dto
-        /// </summary>
-        /// <typeparam name="T">Identifier or detail Dto</typeparam>
-        /// <param name="patientId">Parent resource id to search by</param>
-        /// <param name="id">Resource id to search by</param>
-        /// <returns></returns>
-        private async Task<T> GetPatientLabTestAsync<T>(long patientId, long id) where T : class
-        {
-            var patientLabTestFromRepo = await _patientLabTestRepository.GetAsync(pc => pc.Patient.Id == patientId && pc.Id == id);
-
-            if (patientLabTestFromRepo != null)
-            {
-                // Map EF entity to Dto
-                var mappedPatientLabTest = _mapper.Map<T>(patientLabTestFromRepo);
-
-                return mappedPatientLabTest;
-            }
-
-            return null;
-        }
-
-        /// <summary>
-        ///  Map additional dto detail elements not handled through automapper
-        /// </summary>
-        /// <param name="dto">The dto that the link has been added to</param>
-        /// <returns></returns>
-        private PatientLabTestDetailDto CustomPatientLabTestMap(PatientLabTestDetailDto dto)
-        {
-            var patientLabTest = _patientLabTestRepository.Get(p => p.Id == dto.Id);
-            if (patientLabTest == null)
-            {
-                return dto;
-            }
-            IExtendable patientLabTestExtended = patientLabTest;
-
-            // Map all custom attributes
-            dto.LabTestAttributes = _modelExtensionBuilder.BuildModelExtension(patientLabTestExtended)
-                .Select(h => new AttributeValueDto()
-                {
-                    Key = h.AttributeKey,
-                    Value = h.TransformValueToString(),
-                    Category = h.Category,
-                    SelectionValue = (h.Type == CustomAttributeType.Selection) ? GetSelectionValue(h.AttributeKey, h.Value.ToString()) : string.Empty
-                }).Where(s => (s.Value != "0" && !String.IsNullOrWhiteSpace(s.Value)) || !String.IsNullOrWhiteSpace(s.SelectionValue)).ToList();
-
-            return dto;
-        }
-
-        /// <summary>
-        ///  Prepare HATEOAS links for a single resource
-        /// </summary>
-        /// <param name="dto">The dto that the link has been added to</param>
-        /// <returns></returns>
-        private PatientLabTestIdentifierDto CreateLinksForPatientLabTest<T>(T dto)
-        {
-            PatientLabTestIdentifierDto identifier = (PatientLabTestIdentifierDto)(object)dto;
-
-            identifier.Links.Add(new LinkDto(_linkGeneratorService.CreateResourceUri("PatientLabTest", identifier.Id), "self", "GET"));
-
-            return identifier;
-        }
-
-        /// <summary>
-        /// Get the corresponding selection value
-        /// </summary>
-        /// <param name="attributeKey">The custom attribute key look up value</param>
-        /// <param name="selectionKey">The selection key look up value</param>
-        /// <returns></returns>
-        private string GetSelectionValue(string attributeKey, string selectionKey)
-        {
-            var selectionitem = _selectionDataItemRepository.Get(s => s.AttributeKey == attributeKey && s.SelectionKey == selectionKey);
-
-            return (selectionitem == null) ? string.Empty : selectionitem.Value;
         }
     }
 }
